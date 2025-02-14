507d4a3e34be2622eaf29c58759834f1dee43416
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-14 16:04:55 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let waarde_van_het_kompas = 0
+basic.forever(function () {
+    if (waarde_van_het_kompas == 0) {
+    	
+    }
+})
+basic.forever(function () {
+    if (waarde_van_het_kompas < 136) {
+        basic.showArrow(ArrowNames.East)
+    }
+})
+basic.forever(function () {
+    if (waarde_van_het_kompas < 226) {
+        basic.showArrow(ArrowNames.South)
+    }
+})
